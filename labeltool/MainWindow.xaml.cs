65419dd965ed2e1a4dfec10cd816aaf4e57a6d15
Alex Braun <alex.braun@tum.de>
2018-03-06 17:41:33 +0100

    initial commit

---
+++ labeltool/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using ListView = System.Windows.Controls.ListView;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace labeltool
+{
+    /// <inheritdoc>
+    ///     <cref></cref>
+    /// </inheritdoc>
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private class MyLabelData
+        {
+            public string MyUrl { get; set; }
+            public string MyMultipolygon { get; set; }
+            public List<string> RawPolygons { get; set; }
+        }
+
+        private List<MyLabelData> _myLabels;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void ImportJson(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myFileDialog = new OpenFileDialog
+            {
+                Filter = "JSON schema|*.json",
+                Title = "Load JSON SCHEMA"
+            };
+            if (myFileDialog.ShowDialog() != true)
+            {
+                return;
+            }
+
+            string myJsonSchema = File.ReadAllText(myFileDialog.FileName);
+
+            OpenFileDialog myFileDialog2 = new OpenFileDialog
+            {
+                Filter = "Labelbox JSON Export|*.json",
+                Title = "Import an exported JSON from labelbox.io"
+            };
+            if (myFileDialog2.ShowDialog() != true)
+            {
+                return;
+            }
+
+            string myJson = File.ReadAllText(myFileDialog2.FileName);
+
+            JSchema schema = JSchema.Parse(myJsonSchema);
+            dynamic myLabels = JsonConvert.DeserializeObject(myJson);
+
+            JToken json = JToken.Parse(myJson);
+
+            bool isValid = json.IsValid(schema);
+            if (!isValid)
+            {
+               return;
+            }
+
+            _myLabels = new List<MyLabelData>();
+            foreach (dynamic myLabel in myLabels)
+            {
+                string thisUrl = myLabel["Labeled Data"].ToString();
+                string curLabel = null;
+                List<string> rawLabels = new List<string>();
+                foreach (dynamic multilabel in myLabel.Label)
+                {
+                    string labeltitle = multilabel.Name.ToString();
+
+                    string thisMultipolygon = multilabel.Value.ToString();
+                    string thisPolygon = thisMultipolygon.Replace("MULTIPOLYGON (", "");
+                    thisPolygon = thisPolygon.Replace("((", "");
+                    thisPolygon = thisPolygon.Replace(")), ", ";");
+                    thisPolygon = thisPolygon.Replace(")", "");
+                    string[] polyArray = thisPolygon.Split(';');
+                    foreach (string thisPoly in polyArray)
+                    {
+                        if (curLabel != null)
+                        {
+                            curLabel += Environment.NewLine;
+                        }
+                        curLabel += labeltitle + " " + thisPoly.Replace(",", "");
+                        rawLabels.Add(thisPoly.Replace(", ",","));
+                    }
+                }
+
+                _myLabels.Add(new MyLabelData
+                {
+                    MyUrl = thisUrl,
+                    MyMultipolygon = curLabel,
+                    RawPolygons = rawLabels
+                });
+            }
+
+            LabelList.ItemsSource = _myLabels;
+        }
+
+        private void CreateFolderStructure(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog myDiag = new FolderBrowserDialog();
+            DialogResult result = myDiag.ShowDialog();
+            if (result != System.Windows.Forms.DialogResult.OK) return;
+            if (_myLabels == null) return;
+
+            string folderName = myDiag.SelectedPath;
+
+
+        }
+
+        private void LabelList_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MyLabelData selectedLabel = (MyLabelData)((ListView)sender).SelectedItem;
+            if (selectedLabel == null) return;
+
+            Pictureviewer pictureviewer = new Pictureviewer(selectedLabel.MyUrl, selectedLabel.RawPolygons);
+            pictureviewer.Show();
+        }
+    }
+}
---
+++ labeltool/pictureviewer.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Brush = System.Windows.Media.Brush;
+using Brushes = System.Windows.Media.Brushes;
+using Point = System.Windows.Point;
+
+namespace labeltool
+{
+    /// <inheritdoc cref="" />
+    /// <summary>
+    /// Interaktionslogik für pictureviewer.xaml
+    /// </summary>
+    public partial class Pictureviewer
+    {
+        private string MyTitle
+        {
+            set => SetValue(MyTitleProperty, value);
+        }
+
+        public static readonly DependencyProperty MyTitleProperty =
+            DependencyProperty.Register("MyTitle", typeof(string), typeof(Pictureviewer), new UIPropertyMetadata(null));
+
+        private const int MyWidth = 800;
+        private const int MyHeight = 600;
+        private readonly Brush _myStroke = Brushes.Lime;
+        private const double MyStrokeThickness = 2;
+
+
+        public Pictureviewer(string myUrl, List<string> myLabels)
+        {
+            InitializeComponent();
+
+            SizeToContent = SizeToContent.WidthAndHeight;
+            DataContext = this;
+            MyTitle = myUrl;
+            
+            Bitmap bitmap = LoadBitmap(myUrl);
+            BitmapImage img = ConvertBitmapImage(bitmap);
+            
+            ImageBrush ib = new ImageBrush
+            {
+                ImageSource = img
+            };
+            Grid.Background = ib;
+            double imgWidth = img.PixelWidth;
+            double imgHeight = img.PixelHeight;
+
+            double imgratiox = MyWidth / imgWidth;
+            double imgratioy = MyHeight / imgHeight;
+
+            foreach (string label in myLabels)
+            {
+                Polygon mypolygon = new Polygon();
+                string[] ptlist = label.Split(',');
+                foreach (string curpt in ptlist)
+                {
+                    mypolygon.Points.Add(PointParser(curpt,imgratiox, imgratioy));
+                }
+                mypolygon.Stroke = _myStroke;
+                mypolygon.StrokeThickness = MyStrokeThickness;
+                Canvas.Children.Add(mypolygon);
+            }
+        }
+
+        private static Bitmap LoadBitmap(string url)
+        {
+            WebClient wc = new WebClient();
+
+            byte[] originalData = wc.DownloadData(url);
+            MemoryStream stream = new MemoryStream(originalData);
+            return new Bitmap(stream);
+        }
+
+
+        private static Point PointParser(string pts, double imgratiox, double imgratioy)
+        {
+            string[] xyStrings = pts.Split(new char[0]);
+            double x = double.Parse(xyStrings[0], NumberStyles.Any, CultureInfo.InvariantCulture);
+            double y = double.Parse(xyStrings[1], NumberStyles.Any, CultureInfo.InvariantCulture);
+            x = x * imgratiox;
+            y = MyHeight - y * imgratioy;
+            return new Point(x, y);
+        }
+
+        private static BitmapImage ConvertBitmapImage(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            src.Save(ms, ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+    }
+}
